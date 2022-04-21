8aa2497d9c43d77a68396effff6b583973f2953b
Dax2976 <ferrodavide55@gmail.com>
2022-04-21 15:47:14 +0200

    push con probelma layout

---
+++ assets/js/app.js
@@ -0,0 +1,51 @@
+const app = new Vue({
+    el:'#app',
+    data:{
+        photo:[
+            {
+                image: './img/01.jpg',
+                title: 'Svezia',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            },
+            {
+                image: './img/02.jpg',
+                title: 'Svizzera',
+                text: 'Lorem ipsum.',
+            },
+            {
+                image: './img/03.jpg',
+                title: 'Gran Bretagna',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            },
+            {
+                image: './img/04.jpg',
+                title: 'Germania',
+                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+            },
+            {
+                image: './img/05.jpg',
+                title: 'Paradise',
+                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis.',
+            }
+        ],
+
+        activeItem:0,
+        
+    },
+    methods:{
+        upImg(){
+            if(this.image > 0){
+                this.image++
+            } else if(this.image == 0){
+                this.image = this.photo.lenght - 1
+            }
+        },
+        downImg(){
+            if(this.image < this.photo.lenght - 1){
+                this.image++
+            } else if(this.image == this.photo.lenght - 1){
+                this.image = 0
+            }
+        }
+    }
+})
